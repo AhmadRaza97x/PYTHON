9c809ca102f96d7dabd719073d83094f90663b2e
MOHAMMAD AHMAD RAZA <ahmadraza>
2025-08-02 13:34:37 +0530

    initial commit

--- BASIC/Main.py
+++ BASIC/Main.py
@@ -1,4 +1,9 @@
-print("HELLO WORLD",7)
+print("\nHELLO WORLD",7)
 print(5)
 print("Bye")
-print(8+5)
+print(8+5)  # PYTHON USE AS CALCULATOR
+print("AHMAD\nRAZA")
+print("I AM A GOOD \"STUDENT\"")    # BY (\\) WE CAN PRINT ANY UN USABLE SYMBOL LIKE COMMA
+print("AZAMGARH",276001,"U.P", sep=" ~ ")   # SEPERATE DIFFERENT CHARACTER BY GIVEN SYMBOL
+print("B.tech",end="(CSE)")
+print(" DELHI")
---
+++ BASIC/Variable.py
@@ -0,0 +1,6 @@
+a = 2
+b = "AHMAD"
+c = True
+d = None
+print(a,b,c,d)
+# print(type(a),type(b),type(c),type(d))
