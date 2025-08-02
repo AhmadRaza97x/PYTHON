9057dd4b61bbd865830bc0e9e8c4780413795759
MOHAMMAD AHMAD RAZA <ahmadraza>
2025-08-02 14:20:51 +0530

    initiaila commit

---
+++ BASIC/3.Operator.py
@@ -0,0 +1,7 @@
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15%6)     # GIVES REMAINDER
+print(15//6)    # GIVES INTEGER VALUE AFTER DIVISION  
+print(5**3)     # GIVES 5 KI POWER 3
---
+++ EXCERCISE/1.Calculator.py
@@ -0,0 +1,6 @@
+a = 15
+b = 6
+print("The value of",a,"+",b,"is:",a+b)
+print("The value of",a,"-",b,"is:",a-b)
+print("The value of",a,"*",b,"is:",a*b)
+print("The value of",a,"/",b,"is:",a/b)
