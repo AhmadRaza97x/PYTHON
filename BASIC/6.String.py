2db9d4ee5f7c199f0360a6a707a4b8a1f62c8fe8
MOHAMMAD AHMAD RAZA <ahmadraza97x@gmail.com>
2025-08-20 20:18:21 +0530

    initial commit

---
+++ BASIC/6.String.py
@@ -0,0 +1,18 @@
+a = "Ahmad"
+b = 'raza'
+print(a + b)
+
+c = '''md
+siraj
+misbahi'''
+print(c)
+
+print(a[0]) # IT PRINT h
+print(a[1]) # IT LIKE CHARACTER ARRAY
+print(a[2])
+print(a[3])
+print(a[4])
+print("\n")
+
+for character in b:
+    print(character)
