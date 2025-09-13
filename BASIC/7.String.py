60c1fe69fccfdcd0a6fc2d5a945c5e152ee660ae
AhmadRaza97x <ahmadraza97x@gmail.com>
2025-09-13 21:46:36 +0530

    initial commit

--- BASIC/7.String.py
+++ BASIC/7.String.py
@@ -1,13 +1,19 @@
 fruit = "mango"
 fruitlen = len(fruit)
 
-print(len(fruit))  # COUNT NO. OF CHARACTER
-print(fruitlen)
+print(len(fruit))   # LEN() COUNT NO. OF CHARACTER
+print(fruitlen)     
 print(fruit)
+print("\n")
 
-print(fruit[0:4])  # IT PRINT UP TO 4 CHARACTER 
-print(fruit[:4])   # BY DEFAULT USE 0
+print(fruit[0:4])   # IT PRINT UP TO 4 CHARACTER 
+print(fruit[:4])    # BY DEFAULT USE 0
 print(fruit[1:4])  
-print(fruit[3])    # IT PRINT 3RD CHARACTER 
-
+print(fruit[3])     # IT PRINT 3RD CHARACTER 
+print(fruit[:])     # SAME AS [0:4]
+print(fruit[0:-3])  # MEANS [0:LEN(FRUIT)-3] OR [0:5-3] OR [0:2]
+print(fruit[-3:-1])   # MEANS [2:4]
+print(fruit[-1:-3])   # MEANS [4:2] NOTHING HAPPEN
+print("\n")
+ 
 print("APPLE is a",len("APPLE"),"character word")
