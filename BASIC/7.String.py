8474df7e437a2654a9a66a620cff3c758d34021d
AhmadRaza97x <ahmadraza97x@gmail.com>
2025-09-13 21:22:33 +0530

    initial commit

---
+++ BASIC/7.String.py
@@ -0,0 +1,13 @@
+fruit = "mango"
+fruitlen = len(fruit)
+
+print(len(fruit))  # COUNT NO. OF CHARACTER
+print(fruitlen)
+print(fruit)
+
+print(fruit[0:4])  # IT PRINT UP TO 4 CHARACTER 
+print(fruit[:4])   # BY DEFAULT USE 0
+print(fruit[1:4])  
+print(fruit[3])    # IT PRINT 3RD CHARACTER 
+
+print("APPLE is a",len("APPLE"),"character word")
